3ef3d9d9864651dae4902e67e7ef8cc4c5c1843c
AlexKira <60853187+AlexKira@users.noreply.github.com>
2020-08-27 14:24:26 +0300

    test

---
+++ models.py
@@ -0,0 +1,39 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+# Create your models here.
+class Article(models.Model):
+    class Meta():
+        db_table = 'article'
+
+    article_title = models.CharField(max_length=200)
+    article_text = RichTextField()
+    article_date = models.DateField()
+    article_likes = models.IntegerField(default=0)
+    article_image = models.ImageField(null=True, blank=True,upload_to="images/",
+        verbose_name=u'Изображение',)
+
+
+    def __unicode__(self):
+        return self.article_title
+
+    def bit (self):
+        if self.article_image:
+            return u'<img src="%s" width="70"/>'% self.article_image.url
+        else:
+            return u'(none)'
+    bit.short_descriptio = u'Изображение'
+    bit.allow_tags = True
+
+
+
+class Comments(models.Model):
+    class Meta():
+        db_table = 'comments'
+
+    comments_text = models.TextField(verbose_name="Текст комментария")
+    comments_date = models.DateField(u'date',auto_now=True)
+    comments_article = models.ForeignKey(Article)
+    comments_author = models.ForeignKey(User)
---
+++ urls.py
@@ -0,0 +1,27 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, include, url
+from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns = patterns('',
+                       # Examples:
+                       # url(r'^$', 'firstapp.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
+
+                       url(r'^1/', 'article.views.basic_one'),
+                       url(r'^2/', 'article.views.template_two'),
+                       url(r'^3/', 'article.views.template_three_simple'),
+                       url(r'^articles/all/$', 'article.views.articles'),
+                       url(r'^articles/get/(?P<article_id>\d+)/$', 'article.views.article'),
+                       url(r'^articles/addlike/(?P<article_id>\d+)/$', 'article.views.addlike'),
+                       url(r'^articles/addcomment/(?P<article_id>\d+)/$', 'article.views.addcomment'),
+                       url(r'^page/(\d+)/$', 'article.views.articles'),
+                       url(r'^$', 'article.views.articles'),
+
+                       )
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns() + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
